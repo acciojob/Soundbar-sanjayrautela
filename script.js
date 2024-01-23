814f9ecff11b7bf2b6c6163cf2cbf703aa4d01fa
Sanjay Rautela <140192191+sanjayrautela@users.noreply.github.com>
2024-01-23 15:33:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function playSound(soundFileName) {
+  const audio = new Audio(`sounds/${soundFileName}`);
+  audio.play();
+}
+
+function stopAllSounds() {
+  const allAudioElements = document.querySelectorAll('audio');
+  allAudioElements.forEach(audio => audio.pause());
+}
+
